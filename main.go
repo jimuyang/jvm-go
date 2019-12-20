d573e363df8579bb1d1bd07c441d1a98140f03c7
杨帆 <fan.yang19@ele.me>
2019-12-20 16:33:13 +0800

    feat: classpath相关实现和读取class文件

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"jvm-go/classpath"
+	"strings"
+)
 
 func main() {
 	cmd := parseCmd()
@@ -15,4 +19,12 @@ func main() {
 
 func startJVM(cmd *Cmd) {
 	fmt.Printf("classpath:%s class:%s args:%v \n", cmd.cpOption, cmd.class, cmd.args)
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	classData, entry, err := cp.ReadClass(className)
+	if err != nil {
+		fmt.Printf("cannot find or load main class %s \n", cmd.class)
+	}
+	fmt.Printf("class data: %v \nentry: %s\n", classData, entry)
 }
