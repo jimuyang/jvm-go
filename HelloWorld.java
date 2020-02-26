238d76a63c7a2385d0109e7908923387186fab84
杨帆 <fan.yang19@ele.me>
2020-02-26 17:41:05 +0800

    calc circular length ;

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,16 @@
 // package main;
 
 public class HelloWorld{
+
+    /**
+     * 计算圆形周长
+     */ 
+    public static float circumference(float r) {
+        float pi = 3.14f;
+        float area = 2 * pi * r;
+        return area;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
